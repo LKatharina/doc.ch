bd8055df2fcaee840b5873f98ec30221d2c366e3
ezeet <64036304+ezeet@users.noreply.github.com>
2021-12-21 09:48:27 +0100

    for synch reasons

--- Execute.R
+++ Execute.R
@@ -5,6 +5,7 @@ library(stringr)
 library(data.table)
 library(cognitivemodels)
 library(tidybayes)
+library(patchwork)
 
 # Definition of all relevant parameters
 S1 <- 1
@@ -26,6 +27,7 @@ Seed <- 42
 
 # Let it run!
 source("Sample&Model.R")
+source("Compare&Plot.R")
 
 d1 <- get_sample(s1 = S1, s2 = S2,
                  r1 = R1, r2 = R2,
@@ -38,11 +40,19 @@ d1 <- get_sample(s1 = S1, s2 = S2,
 
 d2 <- get_model(df = d1, ntrials = Ntrials) 
 
-source("Compare&Plot.R")
-
 comparison <- get_comparison(df = d2)
 
 p1 <- get_plot(unique(d2[budget == Budget[1]]$plot_id))
 p2 <- get_plot(unique(d2[budget == Budget[2]]$plot_id))
+
+p1 + plot_spacer() + p2 +
+  plot_layout(guides = "collect", widths = c(.4,.05,.4)) +
+  plot_annotation(caption = "Note: Points = Median, thick line = 66% quantile intervall, thin line = 95% quantile intervall")
+
+plot_name <-  paste0(Beta_n, "_", Dfe_n[1], "-", Dfe_n[2], "_", Budget[1], "-", Budget[2], "_", Subject_n, "_", 
+                     S1, "_", S2, "_", R1, "_", R2, "_", PS1, "_", PR1)
+
+ggsave(filename = paste0("/figures/", plot_name, ".png"), dpi = 800)
+
 
 
