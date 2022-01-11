227cfa180df5fa8f5492503b33bfc16041b1eee6
ezeet <64036304+ezeet@users.noreply.github.com>
2022-01-11 13:49:51 +0100

    ...

--- get_comparison.R
+++ get_comparison.R
@@ -1,7 +1,4 @@
 
-# Compare & Plot
-
-# Function to compare draw-sizes
 get_comparison <- function(df) {
   
   mean_prhv <- d2[, .(prhv_rsft = mean(prhv_rsft)), 
@@ -32,45 +29,3 @@ get_comparison <- function(df) {
                                                        no = "NO")))]
   return(dfe_comp)
 }
-
-# Function to plot predicted proportion of risky choices
-get_plot <- function(i) {
-  d2[, dfe_id := factor(dfe_id)]
-  the_name <- "Sample Size"
-  the_labels <- c("Large (10 per option)", "Small (3 per option)")
-  the_colors <- c("red", "grey25")
-  ggplot(
-    data = d2[plot_id %in% i],
-    mapping = aes(x = trial, y = prhv_rsft, color = dfe_id, fill = dfe_id)) +
-    stat_halfeye( # uses median and QI = quantile interval (also known as the percentile interval or equi-tailed interval)
-      .width = c(.66, 0.95), #use .66, .95 to show 66 and 96% HDI
-      slab_alpha = 0.15,
-      position = position_dodge(width = .09),
-      aes(shape = dfe_id), point_fill = "white") +
-    theme_classic() +
-    scale_fill_manual(
-      values = the_colors,
-      name = the_name, labels = the_labels) +
-    scale_colour_manual(
-      values = the_colors,
-      name = the_name, labels = the_labels) +
-    scale_shape_manual(
-      values = c(16, 21),
-      name = the_name, labels = the_labels) +
-    facet_wrap(~ trial, labeller = label_both, scale = "free_x") +
-    ylab("Predicted Proportion of Risky Choices") +
-    ylim(c(0,1)) +
-    labs(title = "Environment",
-         subtitle = paste("Reach", d2[plot_id %in% i]$budget[1], "in 3 trials"),
-         caption = paste0("Beta_n = ", max(d2$beta_id)/max(d2$subject_id), 
-                          ", Subject_n = ", max(d2$subject_id)/(length(unique(d2$dfe_id))*length(unique(d2$dfe_id))))) +
-    theme(axis.title.x = element_blank(),
-          axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          legend.key.size = unit(.2, 'cm'),
-          legend.key.height= unit(.5, 'cm'),
-          legend.key.width= unit(.5, 'cm'),
-          legend.title = element_text(size = 5),
-          legend.text = element_text(size = 5))
-}
-
--- get_predictions.R
+++ get_predictions.R
@@ -11,35 +11,35 @@ get_predictions <- function(s1, s2 , r1, r2, ps1, pr1, dfe_n, subject_n, budget,
 
 
 
-all_comp <- NULL
-get_predictions2 <- function(values) {
-  d1 <- get_sample(s1 = values$S1, s2 = values$S2,
-                   r1 = values$R1, r2 = values$R2,
-                   ps1 = values$PS1, pr1 = values$PR1,
-                   dfe_n = c(values$Dfe_1, values$Dfe_2),
-                   subject_n = values$Subject_n,
-                   budget = c(values$Budget_1, values$Budget_2),
-                   beta_n = values$Beta_n,
-                   seed = values$Seed)
-  
-  d2 <- get_model(df = d1, ntrials = values$Ntrials)
-  
-  
-  
-  rel_name <- paste0(values$Beta_n, "_", values$Dfe_1, "-", values$Dfe_2, "_", values$Budget_1, "-", 
-                     values$Budget_2, "_", values$Subject_n, "_", values$S1, "_", values$S2, "_", values$R1, "_", 
-                     values$R2, "_", values$PS1, "_", values$PR1)
-  
-  # assign(paste0("comparison_", rel_name), get_comparison(df = d2))
-  new_comp <- get_comparison(df = d2)
-  all_comp <- rbind(all_comp, new_comp)
-  
-  p1 <- get_plot(unique(d2[budget == unique(values$Budget_1)]$plot_id))
-  p2 <- get_plot(unique(d2[budget == unique(values$Budget_2)]$plot_id))
-  
-  p1 + plot_spacer() + p2 +
-    plot_layout(guides = "collect", widths = c(.4,.05,.4)) +
-    plot_annotation(caption = "Note: Points = Median, thick line = 66% quantile intervall, thin line = 95% quantile intervall")
-  
-  ggsave(filename = paste0("plot_", rel_name, ".png"), width = 1920, height = 1080, units = "px")
-}
+# all_comp <- NULL
+# get_predictions2 <- function(values) {
+#   d1 <- get_sample(s1 = values$S1, s2 = values$S2,
+#                    r1 = values$R1, r2 = values$R2,
+#                    ps1 = values$PS1, pr1 = values$PR1,
+#                    dfe_n = c(values$Dfe_1, values$Dfe_2),
+#                    subject_n = values$Subject_n,
+#                    budget = c(values$Budget_1, values$Budget_2),
+#                    beta_n = values$Beta_n,
+#                    seed = values$Seed)
+#   
+#   d2 <- get_model(df = d1, ntrials = values$Ntrials)
+#   
+#   
+#   
+#   rel_name <- paste0(values$Beta_n, "_", values$Dfe_1, "-", values$Dfe_2, "_", values$Budget_1, "-", 
+#                      values$Budget_2, "_", values$Subject_n, "_", values$S1, "_", values$S2, "_", values$R1, "_", 
+#                      values$R2, "_", values$PS1, "_", values$PR1)
+#   
+#   # assign(paste0("comparison_", rel_name), get_comparison(df = d2))
+#   new_comp <- get_comparison(df = d2)
+#   all_comp <- rbind(all_comp, new_comp)
+#   
+#   p1 <- get_plot(unique(d2[budget == unique(values$Budget_1)]$plot_id))
+#   p2 <- get_plot(unique(d2[budget == unique(values$Budget_2)]$plot_id))
+#   
+#   p1 + plot_spacer() + p2 +
+#     plot_layout(guides = "collect", widths = c(.4,.05,.4)) +
+#     plot_annotation(caption = "Note: Points = Median, thick line = 66% quantile intervall, thin line = 95% quantile intervall")
+#   
+#   ggsave(filename = paste0("plot_", rel_name, ".png"), width = 1920, height = 1080, units = "px")
+# }
